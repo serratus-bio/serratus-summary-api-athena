3d592b0001343d5e1ed98487ad0a845a303d4686
Victor Lin <victor@victorl.in>
2021-01-11 11:09:52 -0500

    cleanup

--- src/index.js
+++ src/index.js
@@ -10,5 +10,5 @@ app.use('/sequence', sequence)
 
 var port = process.env.PORT || 3000
 app.listen(port, () =>
-  console.log(`Example app listening on port ${port}!`),
+  console.log(`Server listening on port ${port}.`),
 )
