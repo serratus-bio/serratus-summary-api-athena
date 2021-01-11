1a9494f57b06709ca3316dc79c6f58e675ca7d86
Victor Lin <victor@victorl.in>
2021-01-11 12:04:18 -0500

    make params optional

--- src/routes/family.js
+++ src/routes/family.js
@@ -3,10 +3,10 @@ const router = express.Router()
 const athena = require('../services/athena')
 
 router.get('/:id', async (req, res, next) => {
-  var scoreMin = req.query.scoreMin
-  var scoreMax = req.query.scoreMax
-  var identityMin = req.query.identityMin
-  var identityMax = req.query.identityMax
+  var scoreMin = req.query.scoreMin || 0
+  var scoreMax = req.query.scoreMax || 100
+  var identityMin = req.query.identityMin || 0
+  var identityMax = req.query.identityMax || 100
   try {
     var result = await athena.queryFamily(req.params.id, scoreMin, scoreMax, identityMin, identityMax)
     console.log(result)
